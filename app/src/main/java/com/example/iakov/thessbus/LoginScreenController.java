7a219fe6e8816afe70f7cf02e1a7612071c74822
Nuxteridas <gkoulax33@gmail.com>
2018-11-24 17:52:11 +0200

    Adding Login and Password Field and Login Button

---
+++ app/src/main/java/com/example/iakov/thessbus/LoginScreenController.java
@@ -0,0 +1,10 @@
+package com.example.iakov.thessbus;
+
+import android.widget.Button;
+
+public class LoginScreenController extends  MainActivity{
+    public Button loginBtn;
+    //public TextField usernameField;
+
+
+}
