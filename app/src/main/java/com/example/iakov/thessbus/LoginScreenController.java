403885e80d3024286fdcddd8c797d53099813705
Nuxteridas <gkoulax33@gmail.com>
2018-11-26 20:57:34 +0200

    LoginScreenController

--- app/src/main/java/com/example/iakov/thessbus/LoginScreenController.java
+++ app/src/main/java/com/example/iakov/thessbus/LoginScreenController.java
@@ -1,10 +1,42 @@
 package com.example.iakov.thessbus;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginScreenController extends  MainActivity{
-    public Button loginBtn;
-    //public TextField usernameField;
+public class LoginScreenController extends AppCompatActivity {
 
+    protected Button LoginBtn;
+    protected EditText Username;
+    protected EditText Password;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen_controller);
+        Toast.makeText(LoginScreenController.this,"Hello World",Toast.LENGTH_SHORT).show();
+        LoginBtn = (Button)findViewById(R.id.LoginBtn);
+        Username = (EditText)findViewById(R.id.username);
+        Password = (EditText)findViewById(R.id.password);
+
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Username.getText().toString().equals("admin")&& Password.getText().toString().equals("admin"))
+                {
+                    Intent intent = new Intent(LoginScreenController.this,TestActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(LoginScreenController.this,"User and Password is correct",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(LoginScreenController.this,"User and Password is not correct",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/iakov/thessbus/MainActivity.java
+++ app/src/main/java/com/example/iakov/thessbus/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.iakov.thessbus;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.loginscreen);
+
+        Intent LoginScreen = new Intent(this,LoginScreenController.class);
+        startActivity(LoginScreen);
+
     }
 }
