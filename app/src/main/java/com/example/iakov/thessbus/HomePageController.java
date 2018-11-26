37c92f7a4828da647501b3e8b4f1cfd091b3163b
Nuxteridas <gkoulax33@gmail.com>
2018-11-27 00:09:21 +0200

    Passing Menu Vol. 1

--- app/src/main/java/com/example/iakov/thessbus/HomePageController.java
+++ app/src/main/java/com/example/iakov/thessbus/HomePageController.java
@@ -1,23 +1,44 @@
 package com.example.iakov.thessbus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class HomePageController extends AppCompatActivity {
     protected DrawerLayout mDrawerLayout;
     protected ActionBarDrawerToggle mToggle;
+    protected Button Ticket;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
-        mDrawerLayout= (DrawerLayout)findViewById(R.id.drawerlayout);
+        Drawer();
+        /*mDrawerLayout= (DrawerLayout)findViewById(R.id.drawerlayout);
         mToggle= new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);*/
+
+        Ticket = (Button)findViewById(R.id.ticket);
+
+        Ticket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                    Intent intent = new Intent(HomePageController.this,TestActivity.class);
+                    startActivity(intent);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                    finish();
+
+            }
+        });
     }
 
     @Override
@@ -27,4 +48,15 @@ public class HomePageController extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void Drawer()
+    {
+        mDrawerLayout= (DrawerLayout)findViewById(R.id.drawerlayout);
+        mToggle= new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
 }
--- app/src/main/java/com/example/iakov/thessbus/TestActivity.java
+++ app/src/main/java/com/example/iakov/thessbus/TestActivity.java
@@ -3,11 +3,12 @@ package com.example.iakov.thessbus;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TestActivity extends AppCompatActivity {
+public class TestActivity extends HomePageController {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        Drawer();
     }
 }
