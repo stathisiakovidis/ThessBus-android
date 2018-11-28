b29b650f5246405c65e61b7d4f7847c057068533
Nuxteridas <gkoulax33@gmail.com>
2018-11-28 08:50:54 +0200

    Choices in the menu get selected and adding code to logout button

--- app/src/main/java/com/example/iakov/thessbus/HomePageController.java
+++ app/src/main/java/com/example/iakov/thessbus/HomePageController.java
@@ -1,7 +1,11 @@
 package com.example.iakov.thessbus;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,7 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class HomePageController extends AppCompatActivity {
+public class HomePageController extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     protected DrawerLayout mDrawerLayout;
     protected ActionBarDrawerToggle mToggle;
     protected Button Ticket;
@@ -35,7 +39,7 @@ public class HomePageController extends AppCompatActivity {
                     Intent intent = new Intent(HomePageController.this,TestActivity.class);
                     startActivity(intent);
                     intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-                    finish();
+
 
             }
         });
@@ -56,7 +60,47 @@ public class HomePageController extends AppCompatActivity {
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //navigationView.setCheckedItem(R.id.history);
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.history)
+        {
+            Toast.makeText(this,"This is History", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(id == R.id.settings)
+        {
+            Toast.makeText(this,"This is Setting", Toast.LENGTH_SHORT).show();
+        }
+        else if(id == R.id.about)
+        {
+            Toast.makeText(this,"This is About Page", Toast.LENGTH_SHORT).show();
+        }
+        else if(id == R.id.deposit)
+        {
+            Toast.makeText(this,"This is Deposit", Toast.LENGTH_SHORT).show();
+        }
+        else if(id == R.id.home)
+        {
+            Toast.makeText(this,"This is Home", Toast.LENGTH_SHORT).show();
+        }
+        else if(id == R.id.logout)
+        {
+            //Toast.makeText(this,"This is Logout", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(HomePageController.this, LoginScreenController.class);
+            startActivity(intent);
+            finish();
+        }
+
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
--- app/src/main/java/com/example/iakov/thessbus/TestActivity.java
+++ app/src/main/java/com/example/iakov/thessbus/TestActivity.java
@@ -1,14 +1,22 @@
 package com.example.iakov.thessbus;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class TestActivity extends HomePageController {
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class TestActivity extends HomePageController implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         Drawer();
+
     }
+
+
 }
